a3c25e7725a758ef262413dfbe58835ff9b5628e
LieRabbit <lierabbitxyy@gmail.com>
2018-02-13 12:37:34 +0800

    补充注释

--- src/ch03/Prior.java
+++ src/ch03/Prior.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
  * 3.8
  * 对于一个给定的字符串数组，请找到一种拼接顺序，使所有小字符串拼接成的大字符串是所有可能的拼接中字典序最小的。
  * 给定一个字符串数组strs，同时给定它的大小，请返回拼接成的串。
+ * 关键点：a+b < b+a 则a在前，b在后
  */
 public class Prior {
     public String findSmallest(String[] strs, int n) {
