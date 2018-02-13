d40a1e9614e3203f83e41b9bd7388210a4daf18e
LieRabbit <lierabbitxyy@gmail.com>
2018-02-13 12:35:08 +0800

    字符串替换

--- src/ch03/Replacement.java
+++ src/ch03/Replacement.java
@@ -4,6 +4,7 @@ package ch03;
  * 3.10
  * 请编写一个方法，将字符串中的空格全部替换为“%20”。假定该字符串有足够的空间存放新增的字符，并且知道字符串的真实长度(小于等于1000)，同时保证字符串由大小写的英文字母组成。
  * 给定一个string iniString 为原始的串，以及串的长度 int len, 返回替换后的string。
+ * 关键点：从后往前替换
  */
 public class Replacement {
     public String replaceSpace(String iniString, int length) {
