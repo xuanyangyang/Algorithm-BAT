60ec208386d23eec010a1c7fc801219e170dd7a2
LieRabbit <lierabbitxyy@gmail.com>
2018-02-18 18:11:18 +0800

    添加注释

--- src/ch03/Reverse.java
+++ src/ch03/Reverse.java
@@ -10,14 +10,15 @@ public class Reverse {
         // write code here
         char[] chars = A.toCharArray();
 
+        // 反转整个字符串
         reverse(chars, 0, chars.length - 1);
         int l = -1;
         int r = -1;
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] != ' ') {
-                if (i == 0 || chars[i - 1] == ' ')
+                if (i == 0 || chars[i - 1] == ' ')  // 寻找需要局部反转的单词的起始下标
                     l = i;
-                if (i == chars.length - 1 || chars[i + 1] == ' ')
+                if (i == chars.length - 1 || chars[i + 1] == ' ')  // 寻找需要局部反转的单词的末尾下标
                     r = i;
             }
             if (l != -1 && r != -1) {
@@ -30,6 +31,7 @@ public class Reverse {
         return String.valueOf(chars);
     }
 
+    // 反转字符串
     public void reverse(char[] chars, int start, int end) {
         while (start < end) {
             swap(chars, start, end);
@@ -38,6 +40,7 @@ public class Reverse {
         }
     }
 
+    // 交换
     public void swap(char[] chars, int i, int j) {
         char temp = chars[i];
         chars[i] = chars[j];
