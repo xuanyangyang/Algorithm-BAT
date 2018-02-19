a1cf37f04780498a8ff7bd36d0442f40c9ae0a5b
LieRabbit <lierabbitxyy@gmail.com>
2018-02-19 11:39:52 +0800

    添加视频中的方法

--- src/ch05/Remove.java
+++ src/ch05/Remove.java
@@ -6,6 +6,7 @@ package ch05;
  * 给定带删除的头节点和要删除的数字，请执行删除操作，返回删除后的头结点。链表中没有重复数字
  */
 public class Remove {
+    // 常规删除
     public ListNode removeNode(ListNode pNode, int delVal) {
         // write code here
         if (pNode != null) {
@@ -26,5 +27,39 @@ public class Remove {
         }
 
         return pNode;
+    }
+
+    // 不需要前节点的删除方式，不可删除最后一个节点
+    public ListNode removeNode2(ListNode pNode, int delVal) throws Exception {
+        if (pNode != null) {
+            ListNode node = pNode;
+            while (node != null) {
+                if (node.val == delVal) {
+                    if (node.next == null) {
+                        throw new Exception("不可用本方法删除最后一个节点");
+                    }
+                    node.val = node.next.val;
+                    node.next = node.next.next;
+                    break;
+                }
+                node = node.next;
+            }
+        }
+
+        return pNode;
+    }
+
+    // 视频中的删除方法，不可删除最后一个节点
+    public boolean removeNode(ListNode pNode) {
+        if (pNode == null) {
+            return false;
+        }
+        ListNode next = pNode.next;
+        if (next == null) {
+            return false;
+        }
+        pNode.val = next.val;
+        pNode.next = next.next;
+        return true;
     }
 }
