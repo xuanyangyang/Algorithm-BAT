aef69ef395dd22ef7bd98cfd48293d4ad0d31880
LieRabbit <lierabbitxyy@gmail.com>
2018-02-26 21:02:55 +0800

    找零钱--动态规划

--- src/ch12/Exchange.java
+++ src/ch12/Exchange.java
@@ -18,8 +18,14 @@ public class Exchange {
 //        return process1(money, 0, aim);
 
         // 记忆搜索
-        int[][] map = new int[n + 1][aim + 1];
-        return process2(money, 0, aim, map);
+//        int[][] map = new int[n + 1][aim + 1];
+//        return process2(money, 0, aim, map);
+
+        // 由记忆搜索引出的动态规划
+//        return process3(money, n, aim);
+
+        // 动态规划
+        return process4(money, n, aim);
     }
 
     /**
@@ -34,7 +40,7 @@ public class Exchange {
     public int process1(int[] money, int index, int aim) {
         int res = 0;
         if (index == money.length)
-            res = aim == 0 ? 1 : 0;
+            res = aim == 0 ? 1 : 0; // 是否成功组成aim
         else {
             for (int i = 0; money[index] * i <= aim; i++) {
                 res += process1(money, index + 1, aim - money[index] * i);
@@ -58,11 +64,10 @@ public class Exchange {
         if (index == money.length)
             res = aim == 0 ? 1 : 0;
         else {
-            int row;
+            int row = index + 1;
             int col;
             int cacheWays;
             for (int i = 0; money[index] * i <= aim; i++) {
-                row = index + 1;
                 col = aim - money[index] * i;
                 cacheWays = map[row][col];
                 if (cacheWays != 0) {
@@ -77,10 +82,74 @@ public class Exchange {
         return res;
     }
 
+    /**
+     * 由记忆搜索引出的动态规划
+     * @param money
+     * @param index
+     * @param aim
+     * @return
+     */
+    public int process3(int[] money, int index, int aim) {
+        int[][] dp = new int[money.length][aim + 1];
+
+        for (int i = 0; i < money.length; i++) { // 组成0的方法只有1种那就是不用
+            dp[i][0] = 1;
+        }
+
+        for (int i = 0; i <= aim; i++) { // 只使用money[0]货币的情况下组成i的方法有几种
+            if (i % money[0] == 0)
+                dp[0][i] = 1;
+            else
+                dp[0][i] = 0;
+        }
+
+        for (int i = 1; i < money.length; i++) {
+            for (int j = 1; j <= aim; j++) {
+                for (int k = 0; k * money[i] <= j; k++)
+                    dp[i][j] += dp[i - 1][j - money[i] * k];
+            }
+        }
+
+        return dp[money.length - 1][aim];
+    }
+
+    /**
+     * 优化后的动态规划
+     * @param money
+     * @param index
+     * @param aim
+     * @return
+     */
+    public int process4(int[] money, int index, int aim) {
+        int[][] dp = new int[money.length][aim + 1];
+
+        for (int i = 0; i < money.length; i++) { // 组成0的方法只有1种那就是不用
+            dp[i][0] = 1;
+        }
+
+        for (int i = 0; i <= aim; i++) { // 只使用money[0]货币的情况下组成i的方法有几种
+            if (i % money[0] == 0)
+                dp[0][i] = 1;
+            else
+                dp[0][i] = 0;
+        }
+
+        for (int i = 1; i < money.length; i++) {
+            for (int j = 1; j <= aim; j++) {
+                if (j - money[i] >= 0)
+                    dp[i][j] = dp[i][j - money[i]] + dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        return dp[money.length - 1][aim];
+    }
+
     public static void main(String[] args) {
-        int[] money = {1, 2, 4};
+        int[] money = {1, 2, 5};
         Exchange exchange = new Exchange();
-        int ways = exchange.countWays(money, 3, 3);
+        int ways = exchange.countWays(money, 3, 100);
         System.out.println(ways);
     }
 }
